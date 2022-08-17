609ebbc19a193775829820b56304c5fd7cc53d99
ekdora <dora.egrykiss@gmail.com>
2022-08-17 14:37:41 +0200

    Task01 Object destructuring solution

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,12 +1,20 @@
+/* - Mentsd el külön lastName, firstName és job változókba a fenti objektum 
+property-jeit destructuring használatával!
+- Ha valamelyik nincs megadva, akkor az érték legyen az "unknown" string! */
+
 const user = {
   firstName: 'John',
   lastName: 'Doe',
 };
 
-//
+const {
+  firstName,
+  lastName,
+  job = "unknown",
+} = user;
 
-// export {
-//   firstName,
-//   lastName,
-//   job,
-// };
+export {
+  firstName,
+  lastName,
+  job,
+};
