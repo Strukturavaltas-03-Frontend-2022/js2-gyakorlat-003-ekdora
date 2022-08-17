17f8fea3445bdcb255ab30556c40bfa758c8fe74
ekdora <dora.egrykiss@gmail.com>
2022-08-17 14:44:40 +0200

    Task02 object destructuring l, f ,j variables solution

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,4 +1,19 @@
+// - Mentsd el külön `l`, `f` és `j` változókba a fenti objektum lastName, firstName és job property-jeit! 
+// - Ha valamelyik nincs megadva, akkor az érték legyen az "unknown" string!
+
 const user = {
   firstName: 'John',
   lastName: 'Doe',
+};
+
+const {
+  firstName: f,
+  lastName: l,
+  job: j = "unknown",
+} = user;
+
+export {
+  f,
+  l,
+  j,
 };
